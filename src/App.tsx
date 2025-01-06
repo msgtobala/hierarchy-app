3832793eff8eb052054d1ff1979d9046903191c8
Balaji Venkatraman <rslbalaji@gmail.com>
2025-01-06 09:11:54 +0530

    Add swap header link

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,18 @@ export function App() {
                     >
                       View Levels
                     </NavLink>
+                    <NavLink
+                      to="/view-swap"
+                      className={({ isActive }) =>
+                        `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ml-8 ${
+                          isActive
+                            ? 'border-coral-500 text-gray-900'
+                            : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                        }`
+                      }
+                    >
+                      View Swap Levels
+                    </NavLink>
                   </div>
                   <div className="flex items-center">
                     <button
